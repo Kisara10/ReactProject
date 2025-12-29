e480908379aad2c0d190dd055158c18e97c7834a
Kisara10 <kisara.radev@gmail.com>
2025-12-29 21:51:55 +0530

    created ImageGallery where the main images and thumbnails for the property

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+const ImageGallery = ({picture}) =>{
+   const [mainImage, setMainImage] = useState(picture[0]);
+
+   return (
+       <div className="ImageGallery">
+          <div className="MainImage">    {/*Main Image*/}
+             <img src={mainImage} alt="property" />
+          </div>
+
+          <div className="SubImage">    {/*Sub Images*/}
+             {picture.map((image, index) => (
+                 <img
+                 key={index}
+                 src={image}
+                 alt="property view"
+                 onClick={() => setMainImage(image)}
+                 />
+             ))}
+          </div>
+
+       </div>
+   );
+}
+
+export default ImageGallery;
