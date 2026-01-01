beff9dc236fd05597494a1ddf843705f18547af7
Kisara10 <kisara.radev@gmail.com>
2026-01-01 12:01:35 +0530

    update the searchpage

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SearchBar from "./SearchBar.jsx";
 
 const SearchPage = () => {
+
+    const [properties, setProperties] = useState([]);
+    const [filter, setFilter] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        const fetchProperties = async () => {
+            const response = await fetch("/properties(1).json");
+            const data = await response.json();
+            setProperties(data.properties);
+            setFilter(data.properties);
+        };
+        fetchProperties();
+    }, []);
+
+    const handleSeacrch = (filters) => {
+        setFilter(properties);
+    };
+
+    {/*Add to favourites */}
+    const addToFavorites = (property) => {
+        if (!favorites.find(fav => fav.id === property.id)) {
+            setFavorites([...favorites, property]);
+        }
+    }
+
+
     return (
         <div className="search-page">
             <header className="header">
