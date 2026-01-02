8b4e2f76b28595c9bd982555c7d7d84a7e0db278
Kisara10 <kisara.radev@gmail.com>
2026-01-02 21:30:56 +0530

    Add conditional rendering for Description, added and location

--- src/components/PropertyTabs.jsx
+++ src/components/PropertyTabs.jsx
@@ -6,11 +6,31 @@ const PropertyTabs = ({property}) => {
     return (
         <div className="property-tabs">
 
-            <div className="TabButtons">
+            <div className="Tab-buttons">
                 <button onClick={() => setActiveTab("description")}>Description</button>
                 <button onClick={() => setActiveTab("added")}>Details</button>
                 <button onClick={() => setActiveTab("location")}>Location</button>
             </div>
+
+            <div className="Tab-content">
+                {activeTab === "description" && (
+                    <p>{property.description}</p>
+                )}
+
+                {activeTab === "added" && (
+                    <ul>
+                        <li>
+                            Added:
+                        </li>
+                    </ul>
+                )}
+
+                {activeTab === "location" && (
+                    <p>{property.location}</p>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default PropertyTabs;
